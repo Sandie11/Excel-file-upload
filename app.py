6cba7714ed404426047d9abfbff36bf4f5c00257
Sandie11 <87668502+Sandie11@users.noreply.github.com>
2023-12-07 23:54:10 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,25 +9,64 @@ app = Flask(__name__)
 UPLOAD_FOLDER = 'C:/Users/lenovo/Documents/dj'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
-# MySQL connection settings
-host = 'localhost'
-port = 3306
-user = 'root'
-password = '*****'
-database = 'userdb'
+# MySQL connection details
+db_config = {
+    'host': 'localhost',
+    'user': 'root',
+    'password': '123Aa',
+    'database': 'userdb',
+    'cursorclass': pymysql.cursors.DictCursor
+}
 
-conn = pymysql.connect(
-    host=host,
-    port=port,
-    user=user,
-    password=password,
-    database=database
-)
-
+# Route for the index page
 @app.route('/')
 def index():
     return render_template('index.html')
 
+# Route for the dashboard page
+@app.route('/dashboard')
+def dashboard():
+    # Open a connection to the database
+    connection = pymysql.connect(**db_config)
+
+    try:
+        with connection.cursor() as cursor:
+            # Fetch insights from the database
+            cursor.execute("SELECT COUNT(*) AS total_customers FROM customers")
+            total_customers = cursor.fetchone()['total_customers']
+
+            cursor.execute("SELECT COUNT(*) AS customers_from_usa FROM customers WHERE country = 'USA'")
+            customers_from_usa = cursor.fetchone()['customers_from_usa']
+
+            cursor.execute("SELECT MAX(creditLimit) AS highest_credit_limit FROM customers")
+            highest_credit_limit = cursor.fetchone()['highest_credit_limit']
+
+            cursor.execute("SELECT AVG(creditLimit) AS average_credit_limit FROM customers")
+            average_credit_limit = cursor.fetchone()['average_credit_limit']
+
+            cursor.execute("SELECT COUNT(*) AS customers_no_sales_rep FROM customers WHERE salesRepEmployeeNumber IS NULL")
+            customers_no_sales_rep = cursor.fetchone()['customers_no_sales_rep']
+
+            # Repeat the process for other insights...
+
+    except Exception as e:
+        # Handle exceptions (e.g., log the error, provide a fallback response)
+        print(f"Error: {e}")
+        # You might want to render an error page or provide a meaningful response here.
+
+    finally:
+        # Close the database connection
+        connection.close()
+
+    # Render the HTML template with the fetched data
+    return render_template('dashboard.html', 
+                            total_customers=total_customers, 
+                            customers_from_usa=customers_from_usa,
+                            highest_credit_limit=highest_credit_limit,
+                            average_credit_limit=average_credit_limit,
+                            customers_no_sales_rep=customers_no_sales_rep)
+
+# Route for handling file upload
 @app.route('/upload', methods=['POST'])
 def upload_file():
     file = request.files['file']
@@ -48,20 +87,31 @@ def upload_file():
         return jsonify({'error': 'No file provided'})
 
 def insert_data_to_mysql(df):
-    cursor = conn.cursor()
+    # Open a connection to the database
+    connection = pymysql.connect(**db_config)
 
-    # Assuming 'employees' table exists in your MySQL database
-    table_name = 'employees'
+    try:
+        with connection.cursor() as cursor:
+            # Assuming 'employees' table exists in your MySQL database
+            table_name = 'employees'
 
-    # Construct SQL INSERT statement
-    insert_sql = f"INSERT INTO {table_name} ({', '.join(df.columns)}) VALUES ({', '.join(['%s' for _ in df.columns])})"
+            # Construct SQL INSERT statement
+            insert_sql = f"INSERT INTO {table_name} ({', '.join(df.columns)}) VALUES ({', '.join(['%s' for _ in df.columns])})"
 
-    # Insert each row of data into the MySQL table
-    for _, row in df.iterrows():
-        cursor.execute(insert_sql, tuple(row))
+            # Insert each row of data into the MySQL table
+            for _, row in df.iterrows():
+                cursor.execute(insert_sql, tuple(row))
 
-    conn.commit()
-    cursor.close()
+        # Commit the changes
+        connection.commit()
+
+    except Exception as e:
+        # Handle exceptions (e.g., log the error, provide a fallback response)
+        print(f"Error: {e}")
+
+    finally:
+        # Close the database connection
+        connection.close()
 
 if __name__ == '__main__':
     app.run(debug=True)
